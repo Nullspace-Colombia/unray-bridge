687a4df856fa25d4ab86e110a0a18f5680233a47
Andrés Morales <amoralesma@unal.edu.co>
2023-04-23 15:11:47 -0500

    [New] Console GUI

---
+++ unray_bridge/gui.py
@@ -0,0 +1,23 @@
+
+
+
+def print_title(): 
+    """
+        Print title 
+
+        print a splash screen for 
+    """
+    print("            _    _                       ")
+    print("           | |  | |                      ")
+    print("           | |  | |_ __  _ __ __ _ _   _ ")
+    print("           | |  | | '_ \| '__/ _` | | | |")
+    print("           | |__| | | | | | | (_| | |_| |")
+    print("            \____/|_| |_|_|  \__,_|\__, |")
+    print("                                    __/ |")
+    print("                                   |___/ ")
+    print("                                         ")
+    print("** A RLLib framework for external simulation using Unreal Engine ")
+    print("Created and Mantained by NullSpace")
+    print(" ")
+    print(" -- ")
+    
