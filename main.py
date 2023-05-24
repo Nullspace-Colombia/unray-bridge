724819ff4b5148fae8fdb7bab3b8cb0f6f041c5e
Valentina Hernández <vahernandezmo@unal.edu.co>
2023-05-24 07:59:34 -0500

    Created test for 2 envs

--- main.py
+++ main.py
@@ -11,33 +11,15 @@ from tests.multiagents import multiagents
 from argparse import ArgumentParser
 from unray_bridge.envs.bridge_env import MultiAgentBridgeEnv
 from unray_bridge.envs.spaces import BridgeSpaces
+from unray_bridge.multiagents_config import MultiEnvCreator
+from unray_bridge.envs.envs.MultiAgentArena import get_config
+
 
 import numpy as np
 
-
-
-env_config = {
-    "agent-1":{
-        "observation": BridgeSpaces.MultiDiscrete([64, 64]),
-        "action": BridgeSpaces.Discrete(4),
-        "can_show": 1, # Amount of observations int obs stack
-        "can_see": 2, # Amount of observations required in training 
-        "obs_order": {   
-            "agent-1": [0], 
-            "agent-2": [0]
-        }
-    }, 
-    "agent-2":{
-        "observation": BridgeSpaces.MultiDiscrete([64, 64]),
-        "action": BridgeSpaces.Discrete(4),
-        "can_show": 1, # Amount of observations int obs stack
-        "can_see": 2,
-        "obs_order": {
-            "agent-2": [0], 
-            "agent-1": [0]
-        }
-    }
-}
+single_env = get_config()
+creator = MultiEnvCreator(single_env, amount_of_envs=3)
+env_config = creator.get_multienv_config_dict()
 
 env = MultiAgentBridgeEnv(
     name = "multiagent-arena",
@@ -48,16 +30,26 @@ env = MultiAgentBridgeEnv(
 
 # Actions test 
 action = {
-    'agent-1': np.array([2]),
-    'agent-2': np.array([0])
+    'agent-1:1': np.array([2]),
+    'agent-2:1': np.array([0]),
+    'agent-1:2': np.array([1]),
+    'agent-2:2': np.array([3]),
 }
 action2 = {
-    'agent-1': np.array([2]),
-    'agent-2': np.array([0])
+    'agent-1:1': np.array([2]),
+    'agent-2:1': np.array([0]),
+    'agent-1:2': np.array([1]),
+    'agent-2:2': np.array([3]),
+    'agent-1:3': np.array([2]),
+    'agent-2:3': np.array([0])
 }
 action3 = {
-    'agent-1': np.array([2]),
-    'agent-2': np.array([3])
+    'agent-1:1': np.array([2]),
+    'agent-2:1': np.array([3]),
+    'agent-1:2': np.array([1]),
+    'agent-2:2': np.array([3]),
+    'agent-1:3': np.array([1]),
+    'agent-2:3': np.array([3])
 }
 action4 = {
     'agent-1': np.array([2]),
